014707a0a860e6bf3fb10f81a23eb3f06537cb19
Juan Bustamante <bjuan210302@gmail.com>
2023-01-17 16:46:51 -0500

    rm scripts for vars

--- zinc-reader/unx_env.sh
+++
@@ -1,4 +0,0 @@
-export ZINC_SEARCH_SERVER_URL="http://localhost:4080/api/"
-export ZINC_SEARCH_USER="admin"
-export ZINC_SEARCH_PASSWORD="Complexpass#123"
-export ZINC_SEARCH_INDEX_NAME="enron-index"
