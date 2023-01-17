56d0ec48dc29c445d2434766e641a5d2a7fc7ae3
Juan Bustamante <juan.bustamante@nevu.finance>
2023-01-16 22:22:55 -0500

    added script for linux env vars

---
+++ zinc-reader/unx_env.sh
@@ -0,0 +1,4 @@
+export ZINC_SEARCH_SERVER_URL="http://localhost:4080/api/"
+export ZINC_SEARCH_USER="admin"
+export ZINC_SEARCH_PASSWORD="Complexpass#123"
+export ZINC_SEARCH_INDEX_NAME="enron-index"
