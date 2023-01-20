3fd1f219594ae9e00b931e58787b864bfaf2adb0
Juan Bustamante <bjuan210302@gmail.com>
2023-01-20 07:40:09 -0500

    style changes

--- e-lookup-fe/src/components/EmailExpandedView.vue
+++ e-lookup-fe/src/components/EmailExpandedView.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="bg-slate-300 p-5">
-    <h2 class="text-xl text-center tracking-wider font-semibold">{{ subject }}</h2>
-  </div>
-  <div class="m-4 text-zinc-500">
-    <label class="px-2 text-gray-800 bg-slate-200 rounded-sm">Date:</label>
-    {{ date }}
-  </div>
-  <div class="m-4 text-zinc-500 ">
-    <label class="px-2 text-gray-800 bg-slate-200 rounded-sm">Subject:</label>
-    {{ subject }}
-  </div>
-  <div class="m-4 text-zinc-500">
-    <label class="px-2 text-gray-800 bg-slate-200 rounded-sm">From:</label>
-    {{ from }}
-  </div>
-  <div class="m-4 text-zinc-500">
-    <label class="px-2 text-gray-800 bg-slate-200 rounded-sm">To:</label>
-    {{ to.join('; ') }}
-  </div>
-  <div class="m-4 mt-6">
-    <h3 class="font-bold">Message:</h3>
-    <p>
-      {{ content }}
-    </p>
+  <div>
+    <div class="bg-slate-300 p-5">
+      <h2 class="text-xl text-center tracking-wider font-semibold">{{ subject }}</h2>
+    </div>
+    <div class="m-4 text-zinc-500">
+      <label class="px-2 text-zinc-800 bg-slate-200 rounded-sm">Date:</label>
+      {{ date }}
+    </div>
+    <div class="m-4 text-zinc-500 ">
+      <label class="px-2 text-zinc-800 bg-slate-200 rounded-sm">Subject:</label>
+      {{ subject }}
+    </div>
+    <div class="m-4 text-zinc-500">
+      <label class="px-2 text-zinc-800 bg-slate-200 rounded-sm">From:</label>
+      {{ from }}
+    </div>
+    <div class="m-4 text-zinc-500">
+      <label class="px-2 text-zinc-800 bg-slate-200 rounded-sm">To:</label>
+      {{ to.join('; ') }}
+    </div>
+    <div class="m-4 mt-6">
+      <h3 class="font-bold">Message:</h3>
+      <p>
+        {{ content }}
+      </p>
+    </div>
   </div>
 </template>
 
--- e-lookup-fe/src/components/EmailMiniView.vue
+++ e-lookup-fe/src/components/EmailMiniView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-4 p-4 border border-gray-500" @click="emit('expandEmail')">
-    <div class="text-gray-500 font-bold">{{ subject }}</div>
+    <div class="text-zinc-500 font-bold">{{ subject }}</div>
     <div class="py-1">{{ from }} {{ to }}</div>
     <div class="py-1" v-if="highlightedContent">
       <p v-for="h in highlightedContent" v-html="h"></p>
