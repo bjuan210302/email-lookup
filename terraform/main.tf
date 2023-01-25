7980ae26b51295633b53eca98bddce3e50f586dc
Juan Bustamante <bjuan210302@gmail.com>
2023-01-25 12:27:20 -0500

    allow ssh everywhere

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
